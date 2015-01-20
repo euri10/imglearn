29016b1e6dcf94804ef9eb1dadc5b6b68fb7b445
euri10 <benoit.barthelet@gmail.com>
2015-01-20 22:05:57 +0100

    working image search

--- googcustomsearch.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pprint
-
-url = ('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=barack%20obama')
-
-request = requests.get(url)
-pprint.pprint(request.json())
-# Process the JSON string.
-
