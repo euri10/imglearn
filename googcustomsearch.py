d0290c55df2956bb7a246ec26e053bf603a969fa
euri10 <benoit.barthelet@gmail.com>
2015-01-20 20:09:42 +0100

    more home with bot delete / add

--- googcustomsearch.py
+++ googcustomsearch.py
@@ -1,36 +1,9 @@
-import httplib2
-
-__author__ = 'euri10'
-
-from apiclient.discovery import build
+import requests
 import pprint
 
-def main():
-  # Build a service object for interacting with the API. Visit
-  # the Google APIs Console <http://code.google.com/apis/console>
-  # to get an API key for your own application.
+url = ('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=barack%20obama')
 
-    proxy__user = ''
-    proxy_pass = ''
-    proxy_host = ''
-    httpProxy = httplib2.Http(
-    proxy_info=httplib2.ProxyInfo(proxy_type=httplib2.socks.PROXY_TYPE_HTTP, proxy_host=proxy_host,
-                                  proxy_port=8080, proxy_user=proxy__user,
-                                  proxy_pass=proxy_pass))
-    proxy_info=httplib2.ProxyInfo(proxy_type=httplib2.socks.PROXY_TYPE_HTTP, proxy_host='',
-                                  proxy_port=8080, proxy_user=proxy__user,
-                                  proxy_pass=proxy_pass))
-
-  service = build("customsearch", "v1",
-            developerKey="AIzaSyDRRpR3GS1F1_jKNNM9HCNd2wJQyPG3oN0",  http=httpProxy)
-
-  res = service.cse().list(
-      q='lectures',
-      cx='017576662512468239146:omuauf_lfve',
-    ).execute()
-  pprint.pprint(res)
-
-if __name__ == '__main__':
-  main()
-
+request = requests.get(url)
+pprint.pprint(request.json())
+# Process the JSON string.
 
