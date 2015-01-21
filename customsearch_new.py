7e8b39fd2cf16afa44cade65fb6d56d7bfef31b2
euri10 <benoit.barthelet@gmail.com>
2015-01-21 20:02:14 +0100

    working image search with / without proxy (f*** work firewall...)

--- customsearch_new.py
+++ customsearch_new.py
@@ -6,6 +6,8 @@ import httplib2
 import socks
 
 
+USE_PROXY = False
+
 # import config options, put them in variables for future use
 parser = ConfigParser()
 parser.read('config.ini')
@@ -13,18 +15,22 @@ developperkey = parser.get('google', 'developperkey')
 cx = parser.get('google', 'cx')
 proxyurl = parser.get('proxy_settings', 'requests_proxy')
 
-# hack for making proxy work in python3
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9000, )
-socks.wrapmodule(httplib2)
-httpproxy = httplib2.Http()
-
+# hack for making proxy work in python 3
+# socksify-pybranch
+if USE_PROXY:
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9000, )
+    socks.wrapmodule(httplib2)
+    httpproxy = httplib2.Http()
+else:
+    httpproxy = None
 
 def search_google(search):
     service = build('customsearch', 'v1', developerKey=developperkey, http=httpproxy)
     res = service.cse().list(q=search, cx=cx, searchType='image').execute()
     return [l['link'] for l in res['items']]
 
-def search_duckduckgo(search)
+def search_duckduckgo(search):
+    pass
 
 
 # with open('data.txt', 'w') as outfile:
